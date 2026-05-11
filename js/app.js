5e1543bf02865179267748d0e66133dfff87792d
Nico <nicolas.venegas@gmail.com>
2026-05-11 12:34:21 -0400

    act contenidos

--- js/app.js
+++ js/app.js
@@ -3,3 +3,13 @@ $(document).foundation();
 $(window).scroll(function() {
   $("#badge-acreditacion").css("opacity", 1 - $(window).scrollTop() / 250);
 });
+
+ const lenis = new Lenis()
+    lenis.on('scroll', (e) => {
+      console.log(e)
+    })
+    function raf(time) {
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+    requestAnimationFrame(raf)
