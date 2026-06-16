a67ebea387c241f2e9918740ecff003cf82cfca4
Nico <nicolas.venegas@gmail.com>
2026-06-16 09:57:48 -0400

    move project remote

---
+++ inc/header.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Doctorado en Psicología - Universidad de la Frontera</title>
+  <link rel="stylesheet" href="fonts/stylesheet.css">
+  <link rel="stylesheet" href="css/app.css">
+</head>
+
+<body>
+
+  <div id="badge-acreditacion">
+    <img src="assets/acreditacion.jpg" alt="">
+  </div>
+
+  <div class="title-bar" data-responsive-toggle="responsive-menu" data-hide-for="medium">
+    <button class="menu-icon" type="button" data-toggle="responsive-menu"></button>
+    <div class="title-bar-title">Menu</div>
+  </div>
+
+  <div class="top-bar" id="responsive-menu">
+    <div class="top-bar-left">
+      <ul class="dropdown menu align-middle" data-dropdown-menu>
+        <li class="menu-text"><a href="index.php"><img src="assets/logo-1.png"></a></li>
+        <li><a href="index.php">Portada</a></li>
+        <li class="has-submenu">
+          <a href="page-presentacion.php">Doctorado</a>
+        </li>
+        <li><a href="page-academicos.php">Académicas/os</a></li>
+        <li><a href="page-plandeestudios.php">Plan de Estudios</a></li>
+        <li><a href="page-investigacion.php">Investigación</a></li>
+        <li><a href="page-laboratorios.php">Laboratorios</a></li>
+      </ul>
+    </div>
+    <div class="top-bar-right">
+      <ul class="menu">
+        <li><a href="https://www.instagram.com/docpsiufro/" target="_blank">Instagram</a></li>
+        <li><a href="https://www.youtube.com/@docpsiufro3917/videos" target="_blank">YouTube</a></li>
+      </ul>
+    </div>
+  </div>
